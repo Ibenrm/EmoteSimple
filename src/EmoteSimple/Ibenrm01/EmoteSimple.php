73ba07a91a743d05f7909d2827dea7edf6b2d891
Ibenrm <58776492+Ibenrm@users.noreply.github.com>
2022-02-05 00:49:16 +0700

    Add files via upload

---
+++ src/EmoteSimple/Ibenrm01/EmoteSimple.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace EmoteSimple\Ibenrm01;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerChatEvent;
+
+use EmoteSimple\Ibenrm01\EmotesAPI;
+use EmoteSimple\Ibenrm01\commands\EmoteCommand;
+use EmoteSimple\Ibenrm01\commands\object\EmotesIds;
+
+class EmoteSimple extends PluginBase implements Listener {
+
+    /** @var array[] $emoteAPI */
+    public $emoteAPI = [];
+
+    /** @var EmoteSimple $instance */
+    private static $instance;
+
+    /**
+     * @return void
+     */
+    public function onEnable(): void{
+        $this->getServer()->getCommandMap()->register("emotes", new EmoteCommand($this));
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->loadConfig();
+        $this->loadEmote();
+        self::$instance = $this;
+    }
+
+    public function onDisable(): void {
+        file_put_contents($this->getDataFolder()."/database/emotes.yml", yaml_emit($this->emoteAPI));
+    }
+
+    /**
+     * @return void
+     */
+    public function loadConfig(): void {
+        if(!is_dir($this->getDataFolder())){
+            @mkdir($this->getDataFolder());
+        }
+        if(!is_dir($this->getDataFolder() . "database")){
+            @mkdir($this->getDataFolder() . "database");
+        }
+        $this->saveResource("config.yml");
+        $this->saveResource("/database/emotes.yml");
+    }
+
+    /**
+     * @return void
+     */
+    public function loadEmote(): void {
+        if(!file_exists($this->getDataFolder()."/database/emotes.yml")){
+            new Config($this->getDataFolder()."/database/emotes.yml", Config::YAML);
+            $this->emoteAPI = yaml_parse(file_get_contents($this->getDataFolder()."/database/emotes.yml"));
+        } else {
+            $this->emoteAPI = yaml_parse(file_get_contents($this->getDataFolder()."/database/emotes.yml"));
+        }
+    }
+
+    /**
+     * @return EmoteSimple
+     */
+    public static function getInstance(): EmoteSimple{
+        return self::$instance;
+    }
+
+
+    /**
+     * @return EmotesAPI
+     */
+    public function getEmotes(): EmotesAPI{
+        return new EmotesAPI($this);
+    }
+    
+    public function getDataIds(): EmotesIds {
+    	return new EmotesIds($this);
+    }
+
+    /**
+     * @param string $message
+     * @param array $keys
+     * 
+     * @return string
+     */
+    public function replace(string $message, array $keys): string{
+        foreach($keys as $word => $value){
+            $message = str_replace("{".strtolower($word)."}", $value, $message);
+        }
+        return $message;
+    }
+
+    /**
+     * @param PlayerChatEvent $event
+     */
+    public function onChat(PlayerChatEvent $event){
+        $player = $event->getPlayer();
+        $params = explode(" ", $event->getMessage());
+        switch($params[0]){
+            case "!emotes":
+                $event->cancel();
+                if(!isset($params[1])){
+                    $player->sendMessage("§a> Emotes Commands (1/1)\n".
+                    "§7list : §bList a available emotes\n".
+                    "§7emote_name : §bActivated emotes");
+                    break;
+                }
+                if($params[1] == "list"){
+                    $player->sendMessage("§7===== §l§eLIST EMOTES §r§7=====\n");
+                    foreach($this->emoteAPI as $index => $list) :
+                        $player->sendMessage("§7- §b{$list['name']}\n");
+                    endforeach;
+                    $player->sendMessage("§7===============================");
+                    break;
+                }
+                if($this->getEmotes()->searchEmote($params[1])){
+                    $emoteId = $this->getEmotes()->getIds($this->getEmotes()->getIndex($params[1]));
+                    if($this->getEmotes()->setEmotes($player, $emoteId)){
+                        $player->sendMessage($this->replace($this->getConfig()->get("use.emotes"), [
+                            "emote_name"=>$params[1]
+                        ]));
+                    }
+                }
+                break;
+        }
+    }
+}
---
+++ src/EmoteSimple/Ibenrm01/EmotesAPI.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace EmoteSimple\Ibenrm01;
+
+use pocketmine\Server;
+use pocketmine\player\Player;
+
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+use pocketmine\event\player\PlayerEmoteEvent;
+
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\EmotePacket;
+
+use EmoteSimple\Ibenrm01\EmoteSimple;
+
+class EmotesAPI implements Listener{
+
+    private int $lastEmoteTick = 0;
+
+    /**
+     * EmotesAPI contructor.
+     * @param EmoteSimple
+     */
+    public function __construct(private EmoteSimple $plugin) {
+    }
+
+    /**
+     * @param DataPacketReceiveEvent $event
+     * 
+     * @return void
+     */
+    public function onDataPacketReceived(DataPacketReceiveEvent $event) : void{
+		$packet = $event->getPacket();
+        $player = $event->getPlayer();
+		if($packet instanceof EmotePacket){
+			$emoteId = $packet->getEmoteId();
+            foreach($player->getViewers() as $players){
+                $players->getNetworkSession()->onEmote($player, $emoteId);
+            }
+		}
+	}
+
+    /**
+     * @param string $emoteName
+     * 
+     * @return bool
+     */
+    public function searchEmote(string $emoteName): bool{
+        $index = false;
+        foreach ($this->plugin->emoteAPI as $key => $val) {
+            if ($val["name"] == $emoteName) {
+                $index = true;
+                break;
+            }
+        }
+        if($index !== false){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $emoteName
+     */
+    public function getIndex($emoteName = false){
+        $index = false;
+        if($emoteName == false){
+            return false;
+        }
+        foreach($this->plugin->emoteAPI as $key => $emote){
+            if($emote["name"] == $emoteName){
+                $index = $key;
+                break;
+            }
+        }
+        if($index !== false){
+            return $index;
+        }
+        return false;
+    }
+
+    /**
+     * @param string $emoteName
+     * @param string $uid
+     * 
+     * @return string
+     */
+    public function createEmotes(string $emoteName, string $uid): string{
+        if(!$this->searchEmote($emoteName)){
+            $this->plugin->emoteAPI[] = ["name" => $emoteName, "uid" => $uid];
+            return $this->plugin->replace($this->plugin->getConfig()->get("create.emote-success"), [
+                "emote_name"=>$emoteName,
+                "uid"=>$uid
+            ]);
+        }
+        return $this->plugin->replace($this->plugin->getConfig()->get("create.emote-already"), [
+            "emote_name"=>$emoteName,
+            "uid"=>$uid
+        ]);
+    }
+
+    /**
+     * @param string $emoteName
+     * 
+     * @return string
+     */
+    public function removeEmotes(string $emoteName): string{
+        if($this->searchEmote($emoteName)){
+            array_splice($this->plugin->emoteAPI, $this->getIndex($emoteName), 1);
+            return $this->plugin->replace($this->plugin->getConfig()->get("remove.emote-success"), [
+                "emote_name"=>$emoteName
+            ]);
+        }
+        return $this->plugin->replace($this->plugin->getConfig()->get("remove.emote-already"), [
+            "emote_name"=>$emoteName
+        ]);
+    }
+
+    /**
+     * @param int $index
+     * 
+     * @return string
+     */
+    public function getIds(int $index): string{
+        return $this->plugin->emoteAPI[$index]['uid'];
+    }
+
+    /**
+     * @param Player $player
+     * @param string $emoteId
+     * 
+     * @return bool
+     */
+    public function setEmotes(Player $player, string $emoteId): bool{
+        foreach($player->getViewers() as $players){
+            $players->getNetworkSession()->onEmote($player, $emoteId);
+        }
+        return true;
+    }
+}
